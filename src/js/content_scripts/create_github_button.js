c86f6d037a9062a7ac1e68af6a0a9d1e2c86f661
huynhit-92 <h-pham@starx.co.jp>
2019-05-19 07:16:30 +0900

    new list

--- src/js/content_scripts/create_github_button.js
+++ src/js/content_scripts/create_github_button.js
@@ -6,8 +6,6 @@ let ready = require('document-ready')
 let $ = require('jQuery')
 
 ready(function () {
-  let targetTrs = $("#issues-table tbody tr")
-
   function findInnerTextByColumnName(tr, colName) {
     let element = $(tr).children("td[data-column-name='" + colName + "']")[0]
     if (element) {
@@ -39,30 +37,48 @@ ready(function () {
     return result;
   }
 
-  for (let i = 0; i < targetTrs.length; i++) {
-    // Find td
-    let issueTypeTd = $(targetTrs[i]).children("td[data-column-name='issueType']")[0]
-    let issueKeyTd = $(targetTrs[i]).children("td[data-column-name='issueKey']")[0]
-    let issueSummaryTd = $(targetTrs[i]).children("td[data-column-name='summary']")[0]
-    let issueKey =  $(issueKeyTd).children('a')[0].innerText
+  function onTableChange() {
+    console.log("Fffff");
+    let targetTrs = $("#issues-table tbody tr")
+    for (let i = 0; i < targetTrs.length; i++) {
+      // Find td
+      let issueTypeTd = $(targetTrs[i]).children("td[data-column-name='issueType']")[0]
+      let issueKeyTd = $(targetTrs[i]).children("td[data-column-name='issueKey']")[0]
+      let issueSummaryTd = $(targetTrs[i]).children("td[data-column-name='summary']")[0]
+      let issueKey =  $(issueKeyTd).children('a')[0].innerText
 
-    // Create button
-    let githubLink = document.createElement('a');
-    githubLink.setAttribute('class', 'button button--primary github_issue')
-    githubLink.setAttribute('href', 'javascript:void(0)')
+      // Create button
+      let githubLink = document.createElement('a');
+      githubLink.setAttribute('class', 'button button--primary github_issue')
+      githubLink.setAttribute('href', 'javascript:void(0)')
 
-    githubLink.onclick = function(event){
-      chrome.storage.sync.get([GITHUB_URL_KEY, GITHUB_TITLE_KEY, GITHUB_BODY_KEY], function(result){
-        let url = result[GITHUB_URL_KEY]
-        let title = result[GITHUB_TITLE_KEY] || GITHUB_TITLE_TEMPLATE
-        let body = result[GITHUB_BODY_KEY] || GITHUB_BODY_TEMPLATE
+      githubLink.onclick = function(event){
+        chrome.storage.sync.get([GITHUB_URL_KEY, GITHUB_TITLE_KEY, GITHUB_BODY_KEY], function(result){
+          let url = result[GITHUB_URL_KEY]
+          let title = result[GITHUB_TITLE_KEY] || GITHUB_TITLE_TEMPLATE
+          let body = result[GITHUB_BODY_KEY] || GITHUB_BODY_TEMPLATE
 
-        title = replaceSpecialChar(convertToText(title, targetTrs[i]))
-        body = replaceSpecialChar(convertToText(body, targetTrs[i]))
-        window.open(`${url}/issues/new?title=${title}&body=${body}`)
-      })
+          title = replaceSpecialChar(convertToText(title, targetTrs[i]))
+          body = replaceSpecialChar(convertToText(body, targetTrs[i]))
+          window.open(`${url}/issues/new?title=${title}&body=${body}`)
+        })
+      }
+
+      issueKeyTd.appendChild(githubLink)
     }
-
-    issueKeyTd.appendChild(githubLink)
   }
+
+  onTableChange();
+
+  // observer
+  let target = document.getElementById("issues-table");
+  let observere = new MutationObserver((mutations) => {
+    onTableChange();
+  });
+  let config = {
+    characterData: true,
+    childList: true
+  };
+
+  observere.observe(target, config);
 })
